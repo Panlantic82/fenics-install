904b6b154b3363a345c442e67544a0cc248c59d3
panlantic <panlantic82@gmail.com>
2018-02-24 13:28:36 +0900

    first commit

---
+++ fenics.sh
@@ -0,0 +1,19 @@
+sudo apt-get update -y
+sudo apt-get install python3 python3-pip -y
+sudo apt-get install git -y
+sudo apt-get install cmake -y
+git clone https://bitbucket.org/fenics-project/fiat
+git clone https://bitbucket.org/fenics-project/instant
+git clone https://bitbucket.org/fenics-project/dijitso
+git clone https://bitbucket.org/fenics-project/ufl
+git clone https://bitbucket.org/fenics-project/ffc
+git clone https://bitbucket.org/fenics-project/dolfin
+git clone https://bitbucket.org/fenics-project/mshr
+cd fiat    && sudo pip3 install .
+cd ../instant && sudo pip3 install .
+cd ../dijitso && sudo pip3 install .
+cd ../ufl     && sudo pip3 install .
+cd ../ffc     && sudo pip3 install .
+cd ../dolfin  && mkdir -p build && cd build && cmake .. && make install
+cd ..
+cd ../mshr    && mkdir -p build && cd build && cmake .. && make install
