83b8351abaafb78c56659294c12358ed3af68d54
panlantic <panlantic82@gmail.com>
2018-02-24 13:38:32 +0900

    added boost libraries

--- fenics.sh
+++ fenics.sh
@@ -2,6 +2,7 @@ sudo apt-get update -y
 sudo apt-get install python3 python3-pip -y
 sudo apt-get install git -y
 sudo apt-get install cmake -y
+sudo apt-get install libboost-all-dev libmpfr-dev libmpfr-doc libmpfr4 libmpfr4-dbg libeigen3-dev -y
 git clone https://bitbucket.org/fenics-project/fiat
 git clone https://bitbucket.org/fenics-project/instant
 git clone https://bitbucket.org/fenics-project/dijitso
