84edd5040a076dce20dc138743f927f33c91d3cf
Alex Philippov <panlantic82@gmail.com>
2018-03-03 11:55:28 +0900

    use docker instead

--- fenics.sh
+++ fenics.sh
@@ -1,20 +1,10 @@
-sudo apt-get update -y
-sudo apt-get install python3 python3-pip -y
-sudo apt-get install git -y
-sudo apt-get install cmake -y
-sudo apt-get install libboost-all-dev libmpfr-dev libmpfr-doc libmpfr4 libmpfr4-dbg libeigen3-dev -y
-git clone https://bitbucket.org/fenics-project/fiat
-git clone https://bitbucket.org/fenics-project/instant
-git clone https://bitbucket.org/fenics-project/dijitso
-git clone https://bitbucket.org/fenics-project/ufl
-git clone https://bitbucket.org/fenics-project/ffc
-git clone https://bitbucket.org/fenics-project/dolfin
-git clone https://bitbucket.org/fenics-project/mshr
-cd fiat    && sudo pip3 install .
-cd ../instant && sudo pip3 install .
-cd ../dijitso && sudo pip3 install .
-cd ../ufl     && sudo pip3 install .
-cd ../ffc     && sudo pip3 install .
-cd ../dolfin  && mkdir -p build && cd build && sudo cmake .. && sudo make install
-cd ..
-cd ../mshr    && mkdir -p build && cd build && sudo cmake .. && sudo make install
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo usermod -aG docker $USER
+sudo apt-get install paraview gmsh -y
+curl -s https://get.fenicsproject.org | bash
+fenicsproject pull
